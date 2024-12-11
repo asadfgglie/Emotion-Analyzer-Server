af86cf9aca375336d7cd6fb135469f12947fba3a
asadfgglie <ac121878@outlook.com>
2024-12-11 20:28:41 +0800

    add weight arg

--- api.py
+++ api.py
@@ -4,13 +4,13 @@ import time
 import warnings
 
 import fastapi
+import numpy as np
 import torch.cuda
-from torch import float16
 from transformers import pipeline, Pipeline, AutoModelForSequenceClassification, AutoTokenizer
 from typing_extensions import Optional, Union
 
-from schema import AnalyzeRequest, AnalyzeResponse, AnalyzeTestResponse
 import config
+from schema import AnalyzeRequest, AnalyzeResponse, AnalyzeTestResponse
 
 if config.USE_TRANSLATOR:
     from googletrans import Translator
@@ -37,14 +37,43 @@ async def analyze(request: AnalyzeRequest):
     response = pipe(**request.model_dump())
     t3 = time.time()
 
+    def rerank_by_weight(data):
+        if request.weights is None:
+            return
+        tmp = {
+            k:v for k, v in zip(request.candidate_labels, request.weights)
+        }
+
+        weights = np.array([tmp[l] for l in data['labels']])
+        weights_sum = weights.sum()
+        weights_coefficient = weights / weights_sum
+
+        data['scores'] = np.array(data['scores']) * weights_coefficient
+        if not request.multi_label:
+            data['scores'] /= data['scores'].sum()
+        else:
+            data['scores'] *= weights_sum
+        print(data['scores'].sum())
+        ls = sorted(zip(data['labels'], data['scores']), key=lambda x: x[-1], reverse=True)
+        data['labels'] = []
+        data['scores'] = []
+        for l, s in ls:
+            data['labels'].append(l)
+            data['scores'].append(s)
+
     if isinstance(old_seq, list):
         for i, result in enumerate(response):
             result['sequence'] = old_seq[i]
+
+            rerank_by_weight(result)
     else:
         response['sequence'] = old_seq
 
+        rerank_by_weight(response)
+
     logging.info(f'inference time: {t3 - t2}')
     logging.info(f'total time: {t3 - t1}')
+    logging.info(response)
 
     if request.return_testing_data:
         return AnalyzeTestResponse(response=response, inference_time=t3 - t2,
@@ -77,7 +106,6 @@ if __name__ == '__main__':
 
     logging.info('warm up model...')
     pipe(**dict(sequences=["你是在跟我開玩笑嗎?你這該死的傢伙，我要宰了你!"],
-         candidate_labels=["疑惑", "生氣", "害羞", "無語", "自然"],
-         multi_label=True, hypothesis_template="這是一句會使用{}表情說出來的話。"))
+         candidate_labels=["疑惑"], hypothesis_template="這是一句會使用{}表情說出來的話。"))
 
     uvicorn.run(app, host='0.0.0.0', port=20823)
--- schema.py
+++ schema.py
@@ -1,14 +1,25 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, model_validator
 from typing_extensions import Union, Optional
 
 
 class AnalyzeRequest(BaseModel):
     sequences: Union[str, list[str]]
-    candidate_labels: Union[str, list[str]]
+    candidate_labels: list[str]
     hypothesis_template: str = "這是一句會使用{}表情說出來的話。"
     multi_label: bool = False
 
     return_testing_data: bool = False
+    weights: Optional[list[float]] = Field(None, exclude=True)
+
+    @model_validator(mode='after')
+    def format(self):
+        if self.weights is not None:
+            if len(self.weights) != len(self.candidate_labels):
+                raise ValueError('`weights` length should same as candidate_labels number, or weights=None')
+            else:
+                for i in self.weights:
+                    if i <= 0:
+                        raise ValueError('weight should great than zero!')
 
 
 class AnalyzeResponse(BaseModel):
